3bd411252ba95d5ea3ef78e3d6a86ac0cfdcd654
Axsion <ignatevm601@gmail.com>
2026-04-08 14:21:00 +0700

    update progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,56 @@
+#!/usr/bin/emv node
+
+import readlineSync, { question } from 'readline-sync'
+import { greet } from '../src/cli.js'
+
+function progressionQuestion() {
+    const start = Math.floor(Math.random() * 10) + 1
+    const step = Math.floor(Math.random() * 2) + 6
+    const length = Math.floor(Math.random() * 6) + 5
+
+    const progression = []
+    for (let i = 0; i < length; i++) {
+        progression.push(start + i * step)
+    }
+
+    const hiddenIndex = Math.floor(Math.random() * length)
+    const correctAnswer = progression[hiddenIndex]
+
+    const partsQuestion = []
+    for (let i = 0; i < progression; i++) {
+        if (i === hiddenIndex) {
+            partsQuestion.push('..')
+        }
+        else {
+            partsQuestion.push(progression[i])
+        }
+    }
+    return {
+        question: partsQuestion,
+        answer: correctAnswer,
+    }
+}
+
+function playBrainProgression() {
+    console.log('Welcome to the Brain Games!')
+    const userName = greet()
+    console.log('What number is missing in the progression?')
+
+    for (let round = 1; round < 4; round ++) {
+        const { question, answer } = progressionQuestion()
+
+        console.log(`Question: ${question.join('')}`)
+        const userAnswer = readlineSync.question(`Your answer: `)
+
+        if (parseInt(userAnswer) === answer) {
+            console.log('Correct!')
+        }
+        else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`)
+            console.log(`Let's try again, ${userName}!`)
+            return
+        }
+    }
+    console.log(`Congratulations, ${userName}!`)
+}
+playBrainProgression()
